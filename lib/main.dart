429d41a96afb328588863b12d8dd8f3f0e1fbe2e
reer <pigzero7@gmail.com>
2020-11-03 17:31:14 +0900

    アプリライフサイクル

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,54 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
+  //#1
   @override
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  AppLifecycleState _appLifecycleState;
+
+  @override
+  //#1, #3_didChangeAppLifecycleStateからの再開,
+  //inactiveからアプリに戻ってくると通る
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  //#2(端末戻るボタン),　#3_didChangeAppLifecycleStateから戻ってきた時Resumeとして入る
+  //アプリから一旦落とすことなく離脱するとinactiveに入る
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    setState(() {
+      _appLifecycleState = state;
+      print('State = $_appLifecycleState');
+    });
+  }
+
+  //#1
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        body: new Center(
-          child: new Text('Hello'),
-        )
-      )
-    );
+    return new MaterialApp(home: new Scaffold(body: new OrientationBuilder(
+      builder: (context, orientation) {
+        return new Center(
+          child: new Text(
+            _appLifecycleState.toString(),
+            style: new TextStyle(
+                fontSize: 20.0,
+                color: orientation == Orientation.portrait
+                    ? Colors.blue
+                    : Colors.orange),
+          ),
+        );
+      },
+    )));
   }
 }
